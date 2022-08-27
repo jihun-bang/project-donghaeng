628064cb74d9256712631f78bffbc8285b01cd1f
jihun.bang <uwlgnsu@naver.com>
2022-08-28 02:27:01 +0900

    [채팅] 채팅 내용이 가려지는 이슈 수정

--- lib/view/chat_room_view.dart
+++ lib/view/chat_room_view.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:donghaeng/model/chat.dart';
 import 'package:donghaeng/viewmodel/chat_room_viewmodel.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 
 import '../data/di/locator.dart';
@@ -52,7 +52,7 @@ class _ChatroomViewState extends State<ChatroomView> {
     return Consumer<ChatroomViewModel>(
       builder: (_, __, ___) => Scaffold(
         appBar: _appBar,
-        body: Stack(
+        body: Column(
           children: <Widget>[
             _chatMain,
             _sendBar,
@@ -125,47 +125,49 @@ class _ChatroomViewState extends State<ChatroomView> {
         ),
       );
 
-  Widget get _chatMain => ListView.builder(
-      itemCount: chats.length,
-      shrinkWrap: true,
-      padding: const EdgeInsets.all(10),
-      itemBuilder: (context, index) {
-        return Row(
-            mainAxisAlignment: (chats[index].owner == user.uid
-                ? MainAxisAlignment.end
-                : MainAxisAlignment.start),
-            children: <Widget>[
-              Container(
-                  padding: const EdgeInsets.only(
-                      left: 16, right: 12, top: 10, bottom: 10),
-                  child: (chats[index].owner != user.uid)
-                      ? const CircleAvatar(
-                          backgroundImage: NetworkImage(
-                              "https://avatars.githubusercontent.com/u/38811086?v=4"),
-                          maxRadius: 20,
-                        )
-                      : null),
-              Container(
-                padding: const EdgeInsets.only(
-                    left: 16, right: 16, top: 10, bottom: 10),
-                child: Container(
-                  constraints: const BoxConstraints(maxWidth: 299),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.grey.shade200),
-                    color: (chats[index].owner == user.uid
-                        ? Colors.grey.shade200
-                        : Colors.white),
-                  ),
-                  padding: const EdgeInsets.all(16),
-                  child: Text(
-                    chats[index].content,
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                ),
-              )
-            ]);
-      });
+  Widget get _chatMain => Expanded(
+        child: ListView.builder(
+            itemCount: chats.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(10),
+            itemBuilder: (context, index) {
+              return Row(
+                  mainAxisAlignment: (chats[index].owner == user.uid
+                      ? MainAxisAlignment.end
+                      : MainAxisAlignment.start),
+                  children: <Widget>[
+                    Container(
+                        padding: const EdgeInsets.only(
+                            left: 16, right: 12, top: 10, bottom: 10),
+                        child: (chats[index].owner != user.uid)
+                            ? const CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    "https://avatars.githubusercontent.com/u/38811086?v=4"),
+                                maxRadius: 20,
+                              )
+                            : null),
+                    Container(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, top: 10, bottom: 10),
+                      child: Container(
+                        constraints: const BoxConstraints(maxWidth: 299),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.grey.shade200),
+                          color: (chats[index].owner == user.uid
+                              ? Colors.grey.shade200
+                              : Colors.white),
+                        ),
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                          chats[index].content,
+                          style: const TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    )
+                  ]);
+            }),
+      );
 
   Widget get _sendBar => Align(
         alignment: Alignment.bottomLeft,
