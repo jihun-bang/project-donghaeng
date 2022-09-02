0aff020b9190866c1cf3586c1178dce7847320cf
junga park <ja.park@alcherainc.com>
2022-09-02 18:28:44 +0900

    [Chat] 채팅방 만들기-2 view

--- lib/view/chat_room_post_view.dart
+++ lib/view/chat_room_post_view.dart
@@ -1,3 +1,4 @@
+import 'package:donghaeng/view/theme/color.dart';
 import 'package:flutter/material.dart';
 
 class ChatRoomPostView extends StatefulWidget {
@@ -7,7 +8,12 @@ class ChatRoomPostView extends StatefulWidget {
   State<ChatRoomPostView> createState() => _ChatRoomPostView();
 }
 
+const List<String> countries = <String>['영국', '프라하', '크로아티아'];
+
 class _ChatRoomPostView extends State<ChatRoomPostView> {
+  String? country;
+  DateTimeRange? travelDate;
+
   @override
   Widget build(BuildContext context) {
     return _view;
@@ -51,23 +57,108 @@ class _ChatRoomPostView extends State<ChatRoomPostView> {
       );
 
   Widget get _body => Column(
-        children: const <Widget>[
-          SizedBox(
+        children: <Widget>[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _countryDropdownButton,
+              _travelDatePicker,
+            ],
+          ),
+          const SizedBox(
             height: 20,
           ),
-          TextField(
+          const TextField(
               decoration: InputDecoration(
             border: OutlineInputBorder(),
             hintText: "제목을 입력해주세요. (20자 이내)",
           )),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
-          TextField(
+          const TextField(
               decoration: InputDecoration(
             border: OutlineInputBorder(),
             hintText: "내용을 입력해주세요. (20자 이내)",
           )),
         ],
+      );
+
+  void showDateRangePickerPop() async {
+    DateTimeRange? result = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2023),
+      saveText: '완료',
+      builder: (BuildContext context, Widget? child) {
+        return Column(children: [
+          ConstrainedBox(
+            constraints:
+                const BoxConstraints(maxWidth: 500.0, maxHeight: 600.0),
+            child: Theme(data: ThemeData.light(), child: child!),
+          ),
+        ]);
+      },
+    );
+
+    if (result != null) {
+      setState(() {
+        travelDate = result;
+      });
+    }
+  }
+
+  String getTravelDateString(DateTimeRange? dateTimeRange) {
+    if (dateTimeRange == null) {
+      return 'YYYY.MM.DD';
+    }
+
+    return '${dateTimeRange.start.toString().split(' ')[0]} - ${dateTimeRange.end.toString().split(' ')[0]}';
+  }
+
+  Widget get _travelDatePicker => Expanded(
+        child: TextButton(
+            onPressed: () {
+              showDateRangePickerPop();
+            },
+            child: Center(
+                child: Row(children: <Widget>[
+              Icon(
+                Icons.calendar_today,
+                color: MyColors.grey_1,
+              ),
+              Center(
+                child: Text(
+                  getTravelDateString(travelDate),
+                  style: TextStyle(color: MyColors.grey_1),
+                ),
+              )
+            ]))),
+      );
+
+  Widget get _countryDropdownButton => Expanded(
+        child: DropdownButton<String>(
+          value: country,
+          icon: const Icon(Icons.keyboard_arrow_down),
+          elevation: 16,
+          hint: const Center(child: Text("나라 선택")),
+          style: TextStyle(color: MyColors.grey_1),
+          underline: Container(
+            height: 2,
+            color: MyColors.grey_3,
+          ),
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              country = value!;
+            });
+          },
+          items: countries.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
       );
 }
